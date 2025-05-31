f28484d9a07835a7850da3f49e028f1685fdaa3f
vdudouyt <valentin.dudouyt@gmail.com>
2025-05-31 11:06:56 +0300

    Rust sources

---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+//!
+//! `logsy` provides a simple facility for your daily logging tasks.
+//! - [`set_echo`] log into stdout
+//! - [`set_filename`] log into a file (can be combined with [`set_echo`])
+//! - [`set_level`] set log level filter (defaults to `LevelFilter::Info`)
+//!
+//! Calling `set_echo` or `set_filename` for a first time gets our logging handler automatically installed.
+//! # Usage
+//! ```
+//! use log::*;
+//!
+//! logsy::set_echo(true);
+//! logsy::set_filename(Some("logs/main.log")).expect("Couldn't open main.log");
+//! 
+//! info!("Application has just started");
+//! warn!("Dereferencing null pointers harms");
+//! error!("This application got a boo-boo and going to be terminated");
+//! ```
+
+use log::{Record, Level, Metadata, LevelFilter};
+use std::sync::Mutex;
+use std::path::Path;
+use std::fs::{ File, OpenOptions };
+use std::io::Write;
+use chrono::Local;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+
+struct Logsy(Mutex<LogsyConf>);
+
+struct LogsyConf {
+    installed: bool,
+    echo: bool,
+    file: Option<File>,
+    level: Option<Level>,
+}
+
+impl log::Log for Logsy {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        if let Some(level) = self.0.lock().unwrap().level {
+            metadata.level() <= level
+        } else {
+            false
+        }
+    }
+
+    fn log(&self, record: &Record) {
+        if !self.enabled(record.metadata()) {
+            return;
+        }
+        let ts = Local::now().format("%Y-%m-%d %H:%M:%S");
+        let mut conf = self.0.lock().unwrap();
+        let color = match record.metadata().level() {
+            Level::Error | Level::Warn => Color::Red,
+            _ => Color::Green,
+        };
+
+        if conf.echo {
+            let mut stdout = StandardStream::stdout(ColorChoice::Always);
+            print!("[{}][", ts);
+            let _ = stdout.set_color(ColorSpec::new().set_fg(Some(color)));
+            print!("{}", record.level());
+            let _ = stdout.reset();
+            println!("] {}", record.args());
+        }
+        if let Some(file) = &mut conf.file {
+            let _ = writeln!(file, "[{}][{}] {}", ts, record.level(), record.args());
+            let _ = file.flush();
+        }
+    }
+    fn flush(&self) {}
+}
+
+static LOGSY: Logsy = Logsy(Mutex::new(LogsyConf { installed: false, echo: false, file: None, level: None }));
+
+fn check_installed() {
+    let installed = LOGSY.0.lock().unwrap().installed;
+    if !installed {
+       LOGSY.0.lock().unwrap().installed = true;
+       log::set_logger(&LOGSY).unwrap();
+       set_level(LevelFilter::Info);
+    }
+}
+
+/// Start logging into `stdout`
+pub fn set_echo(echo: bool) {
+    check_installed();
+    LOGSY.0.lock().unwrap().echo = echo;
+}
+
+/// Start logging into a specified file.
+/// This function can be called again without restarting the app if you need
+/// (e.g. for implementing log rotation).
+/// If parent dir doesn't exists, it's going to be created.
+pub fn set_filename(filename: Option<&str>) -> Option<()> {
+    check_installed();
+
+    if let Some(filename) = filename {
+        let path = Path::new(filename);
+        let parent_path: &str = path.parent()?.to_str()?;
+        if parent_path.len() > 0 {
+            let result = std::fs::create_dir_all(parent_path);
+            if let Err(err) = result {
+                eprintln!("Couldn't create {}: {}", parent_path, err);
+                return None;
+            }
+            let file = OpenOptions::new().create(true).append(true).open(filename).unwrap();
+            LOGSY.0.lock().unwrap().file = Some(file);
+        }
+    } else {
+        LOGSY.0.lock().unwrap().file = None;
+    }
+
+    Some(())
+}
+
+/// Set log level filter
+pub fn set_level(filter: LevelFilter) {
+    log::set_max_level(filter);
+    LOGSY.0.lock().unwrap().level = filter.to_level();
+}
---
+++ tests/logsy.rs
@@ -0,0 +1,42 @@
+#[cfg(test)]
+mod tests {
+    use tempdir::TempDir;
+    use log::{ info, debug, LevelFilter };
+    #[test]
+    fn test_logsy() {
+        let dir = TempDir::new("my_directory_prefix").unwrap();
+        let filename = dir.path().join("foo.log");
+        logsy::set_filename(Some(filename.to_str().unwrap()));
+        info!("Test123");
+        let contents = std::fs::read_to_string(&filename).unwrap();
+        assert!(contents.contains("Test123"));
+
+        info!("Test125");
+        debug!("Test126");
+        let contents = std::fs::read_to_string(&filename).unwrap();
+        assert!(contents.contains("Test123"));
+        assert!(contents.contains("Test125"));
+        assert!(contents.contains("][INFO]"));
+        assert!(!contents.contains("Test126"));
+        assert!(!contents.contains("][DEBUG]"));
+
+        logsy::set_level(LevelFilter::Debug);
+        debug!("Test126");
+        let contents = std::fs::read_to_string(&filename).unwrap();
+        assert!(contents.contains("Test123"));
+        assert!(contents.contains("Test125"));
+        assert!(contents.contains("Test126"));
+        assert!(contents.contains("][INFO]"));
+        assert!(contents.contains("][DEBUG]"));
+
+        let filename = dir.path().join("foo2.log");
+        logsy::set_filename(Some(filename.to_str().unwrap()));
+        info!("Test127");
+        info!("Test128");
+        info!("Test129");
+        let contents = std::fs::read_to_string(&filename).unwrap();
+        assert!(contents.contains("Test127"));
+        assert!(contents.contains("Test128"));
+        assert!(contents.contains("Test129"));
+    }
+}
